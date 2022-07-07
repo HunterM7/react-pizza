649949bbc221720bc11393d5af4247d0dd85f4d1
Anton Meshchaninov <tonym7@yandex.ru>
2022-07-07 19:21:35 +0300

    finished 6 lesson

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import './scss/app.scss';
 
-import pizzas from './assets/pizzas.json';
-
 import Header from './components/Header';
 import Categories from './components/Categories';
 import Sort from './components/Sort';
 import PizzaCard from './components/PizzaCard';
 
 function App() {
+	const [pizzas, setPizzas] = React.useState([]);
+
+	React.useEffect(() => {
+		fetch('https://62c6ff0674e1381c0a6edc07.mockapi.io/pizzas')
+			.then((res) => {
+				return res.json();
+			})
+			.then((json) => {
+				setPizzas(json);
+			});
+	}, []);
+
 	return (
 		<div className='wrapper'>
 			<Header />
