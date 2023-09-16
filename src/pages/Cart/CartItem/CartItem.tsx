de2210448fbcba94c1a8e1cafba64e8dc8436632
Anton Meshchaninov <tonymsnv@gmail.com>
2023-09-16 21:06:46 +0300

    Updated components

--- src/pages/Cart/CartItem/CartItem.tsx
+++ src/pages/Cart/CartItem/CartItem.tsx
@@ -3,83 +3,75 @@ import React from 'react'
 // Redux
 import { useAppDispatch } from '../../../redux/store'
 import { CartItemType } from '../../../redux/cart/types'
-import {
-	minusItem,
-	plusItem,
-	removeItem,
-} from '../../../redux/cart/slice'
+import { minusItem, plusItem, removeItem } from '../../../redux/cart/slice'
 
 // Files
 import styles from './CartItem.module.scss'
 
-// Components
+// Components 'n UI
+import { RoundButton } from 'ui'
 import { Button } from '../../../components'
 
 const CartItem: React.FC<CartItemType> = ({
-	id,
-	count,
-	imageUrl,
-	price,
-	size,
-	title,
-	type,
+  id,
+  count,
+  imageUrl,
+  price,
+  size,
+  title,
+  type,
 }) => {
-	const dispatch = useAppDispatch()
+  const dispatch = useAppDispatch()
 
-	const onClickPlus = () => {
-		dispatch(plusItem(id))
-	}
+  const onClickPlus = () => {
+    dispatch(plusItem(id))
+  }
 
-	const onClickMinus = () => {
-		dispatch(minusItem(id))
-	}
+  const onClickMinus = () => {
+    dispatch(minusItem(id))
+  }
 
-	const onClickRemove = () => {
-		if (window.confirm('Are you sure?')) {
-			dispatch(removeItem(id))
-		}
-	}
+  const onClickRemove = () => {
+    if (window.confirm('Are you sure?')) {
+      dispatch(removeItem(id))
+    }
+  }
 
-	return (
-		<div className={styles.cartItem}>
-			<div className={styles.info}>
-				<img
-					className={styles.info__img}
-					src={imageUrl}
-					alt='Pizza'
-				/>
+  return (
+    <div className={styles.cartItem}>
+      <div className={styles.info}>
+        <img className={styles.info__img} src={imageUrl} alt="Pizza" />
 
-				<h3 className={styles.info__title}>{title}</h3>
-				<p className={styles.info__desc}>
-					{type} тесто, {size} см.
-				</p>
-			</div>
+        <h3 className={styles.info__title}>{title}</h3>
+        <p className={styles.info__desc}>
+          {type} тесто, {size} см.
+        </p>
+      </div>
 
-			<div className={styles.controls}>
-				<div className={styles.controls__count}>
-					<Button
-						isDisabled={count <= 1}
-						icon='minus'
-						round
-						func={onClickMinus}
-					/>
-					<span>{count}</span>
-					<Button
-						isDisabled={count >= 10}
-						icon='plus'
-						round
-						func={onClickPlus}
-					/>
-				</div>
+      <div className={styles.controls}>
+        <div className={styles.controls__count}>
+          <RoundButton
+            type="minus"
+            onClick={onClickMinus}
+            disabled={count <= 1}
+          />
 
-				<span className={styles.controls__price}>
-					{price * count} ₽
-				</span>
+          <span>{count}</span>
 
-				<Button icon='remove' round func={onClickRemove} />
-			</div>
-		</div>
-	)
+          <Button
+            disabled={count >= 10}
+            icon="plus"
+            round
+            onClick={onClickPlus}
+          />
+        </div>
+
+        <span className={styles.controls__price}>{price * count} ₽</span>
+
+        <Button icon="remove" round onClick={onClickRemove} />
+      </div>
+    </div>
+  )
 }
 
 export default CartItem
